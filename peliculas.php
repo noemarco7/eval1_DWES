613faecb9ac8c3400a23c4a145410e9c4c8312cd
noemarco7 <nmc_1988@hotmail.com>
2020-11-12 20:12:56 +0100

    Update peliculas.php
    Añadir botones y estilos.

--- peliculas.php
+++ peliculas.php
@@ -6,6 +6,21 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap CSS -->
+    <style type="text/css">
+        .boton.editar { 
+            background-color: DodgerBlue;
+            color: #ffffff !important;
+            border: 2px solid #00c69f;
+            border-radius: 7px;
+        }
+        .boton.borrar { 
+            background-color: rgb(255,0,0);
+            color: #ffffff !important;
+            border: 2px solid #00c69f;
+            border-radius: 7px;
+        }
+    </style>
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/estilos.css">
@@ -67,6 +82,30 @@
                     echo "<td></td>";
                     echo "<td></td>";
                     echo "<td><center><b>Primera Plana</b></center></td>";
+                echo "</tr>";
+                echo "<tr>";
+                    echo "<td><center><input type='submit' style='background-color:DodgerBlue;color:white;' value='Editar'/>&nbsp&nbsp<input type='submit' style='background-color:rgb(255,0,0);color:white;' value='Borrar'/></center></td>";
+                    echo "<td></td>";
+                    echo "<td></td>";
+                    echo "<td></td>";
+                    echo "<td></td>";
+                    echo "<td></td>";
+                    echo "<td></td>";
+                    echo "<td><center><input type='submit' style='background-color:DodgerBlue;color:white;' value='Editar'/>&nbsp&nbsp<input type='submit' style='background-color:rgb(255,0,0);color:white;' value='Borrar'/></center></td>";
+                    echo "<td></td>";
+                    echo "<td></td>";
+                    echo "<td></td>";
+                    echo "<td></td>";
+                    echo "<td></td>";
+                    echo "<td></td>";
+                    echo "<td><center><input type='submit' style='background-color:DodgerBlue;color:white;' value='Editar'/>&nbsp&nbsp<input type='submit' style='background-color:rgb(255,0,0);color:white;' value='Borrar'/></center></td>";
+                    echo "<td></td>";
+                    echo "<td></td>";
+                    echo "<td></td>";
+                    echo "<td></td>";
+                    echo "<td></td>";
+                    echo "<td></td>";
+                    echo "<td><center><input type='submit' style='background-color:DodgerBlue;color:white;' value='Editar'/>&nbsp&nbsp<input type='submit' style='background-color:rgb(255,0,0);color:white;' value='Borrar'/></center></td>";
                 echo "</tr>";
            echo "</table>";
             ?>
