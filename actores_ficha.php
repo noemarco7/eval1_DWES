c56ace541c14c31f85b201e37cd2a60220b41d00
noemarco7 <nmc_1988@hotmail.com>
2020-11-20 17:48:56 +0100

    Añade campos a los xxxx_ficha.php
    Se añade los campos a rellenar.

--- actores_ficha.php
+++ actores_ficha.php
@@ -13,10 +13,38 @@
 
 <body>
     <div class="alert alert-secondary d-flex">
-        <a href="./peliculas.php" class="btn btn-dark">Películas</a>&nbsp;&nbsp;
+        <a href="./peliculas_ficha.php" class="btn btn-dark">Listar Películas</a>&nbsp;&nbsp;
     </div>
     <div class="container">
 	<!-- INCLUIR CÓDIGO PHP -->
+
+
+
+    <div class="row mx-auto">
+            <div class="form-row">
+                <div class="col col-md-6">
+                <!--FORMULARIO-->
+                    <form method="post" action="peliculas.php">
+                        <table>
+                            <tr>
+                                <td><b><label>Nombre:</label></b></td>
+                                <td><input class="form form-control" type="text" name="nombre" value="<?php //echo $datos_pelicula[1]; ?>"> </td>
+                            </tr>
+                            <tr>
+                                <td><b><label>Año:</label></b></td>
+                                <td><input class="form form-control" type="text" name="fecha" value="<?php //echo $datos_pelicula[2]; ?>"> </td>
+                            </tr>
+                            <tr>
+                                <td><b><label>País:</label></b></td>
+                                <td>
+                                    <input class="form form-control" type="text" name="duracion" value="<?php //echo $datos_pelicula[3]; ?>">
+                                </td>
+                            </tr>
+                        </table>
+                    </form>
+                </div>
+            </div>
+        </div>
         
     </div>
 </body>
